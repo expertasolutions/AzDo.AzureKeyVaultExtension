0c8367cd0093769941cd6054d261c3aabedda59e
lturmel <lturmel@expertasolutions.com>
2020-04-18 21:59:57 -0400

    error handling review
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- UploadMultipleAzureKeyVaultSecrets/index.ts
+++ UploadMultipleAzureKeyVaultSecrets/index.ts
@@ -77,6 +77,7 @@ async function run() {
           const creds = await LoginToAzure(servicePrincipalId, servicePrincipalKey, tenantId);
           const keyvaultCreds = <TokenCredential> <unknown>(new msRestNodeAuth.ApplicationTokenCredentials(creds.clientId, creds.domain, creds.secret, 'https://vault.azure.net'));
           const keyvaultClient = new msKeyVault.SecretClient(url, keyvaultCreds);
+          console.log(secretsContent.length);
           for(var s=0;s<secretsContent.length;s++){
             let secret = secretsContent[s];
             let secretResult = await keyvaultClient.setSecret(secret.secret, secret.value);
