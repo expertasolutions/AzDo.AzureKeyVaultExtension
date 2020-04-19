8703850b4cc7ab208f8997b6a85c053a4e547ec6
lturmel <lturmel@expertasolutions.com>
2020-04-18 21:52:57 -0400

    output update
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- UploadMultipleAzureKeyVaultSecrets/index.ts
+++ UploadMultipleAzureKeyVaultSecrets/index.ts
@@ -73,13 +73,14 @@ async function run() {
           } catch (err) {
             console.log(err);
           }
-          
+
           const creds = await LoginToAzure(servicePrincipalId, servicePrincipalKey, tenantId);
           const keyvaultCreds = <TokenCredential> <unknown>(new msRestNodeAuth.ApplicationTokenCredentials(creds.clientId, creds.domain, creds.secret, 'https://vault.azure.net'));
           const keyvaultClient = new msKeyVault.SecretClient(url, keyvaultCreds);
           for(var s=0;s<secretsContent.length;s++){
             let secret = secretsContent[s];
             let secretResult = await keyvaultClient.setSecret(secret.secret, secret.value);
+            console.log(secretResult);
             console.log("Secret: " + secret.secret + " Created/Updated");
           }
         } catch (err) {
