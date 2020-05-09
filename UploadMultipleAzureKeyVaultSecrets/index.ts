d470f1fe9ce24e63401e5975386dbaf056d01042
lturmel <lturmel@expertasolutions.com>
2020-05-09 11:25:08 -0400

    add tags on upload multiple secrets
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- UploadMultipleAzureKeyVaultSecrets/index.ts
+++ UploadMultipleAzureKeyVaultSecrets/index.ts
@@ -43,11 +43,19 @@ async function run() {
           const creds = await LoginToAzure(servicePrincipalId, servicePrincipalKey, tenantId);
           const keyvaultCreds = <TokenCredential> <unknown>(new msRestNodeAuth.ApplicationTokenCredentials(creds.clientId, creds.domain, creds.secret, 'https://vault.azure.net'));
           console.log(secretsContent.length);
+
+          let secretOptions: msKeyVault.SetSecretOptions = { 
+            tags: {
+              "env":"dev",
+              "project": "test"
+            }
+          }
+
           const keyvaultClient = new msKeyVault.SecretClient(url, keyvaultCreds);
           console.log("after keyvaultclient");
           for(var s=0;s<secretsContent.length;s++){
             let secret = secretsContent[s];
-            let secretResult = await keyvaultClient.setSecret(secret.secret, secret.value);
+            let secretResult = await keyvaultClient.setSecret(secret.secret, secret.value, secretOptions);
             console.log("Secret: " + secretResult.name + " Created/Updated");
           }
         } catch (err) {
